be79729869bf57aed9232f8f404bbc9e4ff7b4b4
Rayna Jarrett <raynasydonia13@gmail.com>
2026-03-16 22:43:07 -0500

    created migrations folder

--- app/__init__.py
+++ app/__init__.py
@@ -8,9 +8,11 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+from flask_migrate import Migrate 
 # Instantiate Flask-Migrate library here
 
 # Flask-Login login manager
+migrate = Migrate(app, db)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
