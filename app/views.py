ff9596e7569a21b7fbb1b397314744032aa034eb
Rayna Jarrett <raynasydonia13@gmail.com>
2026-03-17 00:48:37 -0500

    Logout successful

--- app/views.py
+++ app/views.py
@@ -100,6 +100,13 @@ def login():
 def load_user(id):
     return db.session.execute(db.select(UserProfile).filter_by(id=id)).scalar()
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'info')
+    return redirect(url_for('home'))
+
 ###
 # The functions below should be applicable to all Flask apps.
 ###
